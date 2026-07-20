37fcc60d0a05bde244c89696caac3131fe818819
iki1uc <entertrainer@web.de>
2026-07-20 06:41:11 +0200

    Add TORrespo configuration object

---
+++ TOR.respo.js
@@ -0,0 +1,13 @@
+const TORrespo = {
+  mode        : "octacore-run",
+  type        : "EVO",
+
+  core        : TOR.pool.core,
+  octacore    : TOR.pool.octacore,
+  four_u      : TOR.pool.4u,
+  onedrive    : TOR.pool.onedrive,
+  verify      : VERIFY,
+
+  output      : "VectorRun",
+  state       : "ready"
+};
